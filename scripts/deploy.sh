7ff6cf1e3010f926f9715f06e79f3f1586197d8f
Ariel Schiavoni <a.schiavoni@2vizcon.com>
2015-11-01 18:26:42 +0100

    Login to npm to deploy package

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ echo "npm install"
 echo "==========================================================="
 npm install --ignore-scripts
 
+echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL"
+
 echo "npm publish"
 echo "==========================================================="
 npm publish
